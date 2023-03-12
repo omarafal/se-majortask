2f8fd8329adeec195fe5298e15daa29609fc784d
Omar-Alaa-Elnahass <21p0197@eng.asu.edu.eg>
2023-03-13 01:45:09 +0200

    Fixed Slow Loading & Added (Add Shoe) feature in admin page

--- Back-end/se_project/shoes/models.py
+++ Back-end/se_project/shoes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Shoe(models.Model):
+    brands_List = (
+        ('1', 'Nike'),
+        ('2', 'Adidas'),
+        ('3', 'Converse'),
+        ('4', 'New Balance')
+    )
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=20, default="Nike", choices=brands_List)
+
+    def __str__(self):
+        return self.name
